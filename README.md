d580d994acc919d65b014fed7f9426d100f939c5
amuhtarov <amuhtarov@akumoproject.com>
2023-08-07 22:53:16 -0500

    test

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # DemoInfra
 Infra for the ami refresh demo
+test
---
+++ terraform/sg.tf
@@ -0,0 +1,20 @@
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2_sg"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
